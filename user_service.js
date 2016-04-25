b9fa3c5b14bf8400cde8086652004cec1042bde4
Hansen <thomas.hansen@yrbrands.com>
2016-04-25 16:43:06 +0200

    new

--- user_service.js
+++ user_service.js
@@ -1,20 +1,36 @@
 var Firebase = require('firebase');
 
 var firebaseRef = new Firebase('https://lot-scraper.firebaseio.com/lots');
+var lots = firebaseRef.child('lots');
+
 
 function addLot(title, link, price, callback) {
-	firebaseRef.set(
-		{
-			link : link,
-			title : title,
-			price : price,
-			date : Firebase.ServerValue.TIMESTAMP
-		}, 
-		function(error) {
-			callback(error);
-		}
-	);
+	lots.once('value', function(snapshot) {
+        if (!snapshot.hasChild(title)) {
+            lots.child(title).set({ title: title });
+        }
+        else {
+            alert("That lot already exists");
+        }
+    });
 }
+
+
+
+
+// function addLot(title, link, price, callback) {
+// 	firebaseRef.push(
+// 		{
+// 			link : link,
+// 			title : title,
+// 			price : price,
+// 			date : Firebase.ServerValue.TIMESTAMP
+// 		}, 
+// 		function(error) {
+// 			callback(error);
+// 		}
+// 	);
+// }
 
 function addUser(email, password, callback) {
 
