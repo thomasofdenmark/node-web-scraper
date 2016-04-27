cc6e9698887a55850ae6fd6f52002a0543b9823b
Hansen <thomas.hansen@yrbrands.com>
2016-04-27 15:03:26 +0200

    roll back rich email body

--- mail_service.js
+++ mail_service.js
@@ -5,10 +5,10 @@ var sendgrid  = require('sendgrid')('SG.v24glhpoSoGiqm_SEu2nRA.n7wXaQqoEAi5FGDYn
 function sendMail(newLot, media, req) {
     if(req.headers.host.lastIndexOf('localhost') == -1) {
         sendgrid.send({
-            to:       'thomasofdenmark@gmail.com',
-            from:     media+'@example.com',
-            subject:  newLot.price+' : '+newLot.title,
-            html:     '<p>'+newLot.title+'<p><p>'+newLot.price+'</p><br><a href="'+newLot.link+'">'+newLot.link+'</a>'
+            to:     'thomasofdenmark@gmail.com',
+            from:   media+'@example.com',
+            subject:newLot.price+' '+newLot.title,
+            html:   '<p>'+newLot.title+'<p><p>'+newLot.price+'</p><br><a href="'+newLot.link+'">'+newLot.link+'</a>'
         }, function(err, json) {
             if (err) { return console.error(err); }
             console.log(json);
